6c2dac77807e42d1139e6a3317be0f088b5622f5
Arne Olemans <Arne.olemans@telenet.be>
2024-10-04 16:33:33 +0100

    Setup Project/Cmake (Love yea matt :pp)

---
+++ ReVengine/ReVengine-Engine/ReVengine.cpp
@@ -0,0 +1,6 @@
+﻿#include "ReVengine.h"
+
+void CmakeTestFunction()
+{
+	std::cout << "Test has been tested :pp" << std::endl;
+}
---
+++ ReVengine/ReVengine-Game/Game.cpp
@@ -0,0 +1,12 @@
+#include "Game.h"
+#include <iostream>
+#include "RevEngine.h"
+
+int main()
+{
+	CmakeTestFunction();
+}
+
+void GameNamNam() {
+	std::cout << "miauw";
+}
